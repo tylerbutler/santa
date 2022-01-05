7606288a10688c4ada6a319563538e653d484a55
Tyler Butler <tyler@tylerbutler.com>
2022-01-04 23:31:08 -0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate clap_verbosity_flag;
+use structopt::StructOpt;
+use std::path::PathBuf;
+use clap::{Parser, Args, Subcommand, ArgEnum};
+
+/// Manage default sets of packages for a variety of package managers.
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+struct Cli {
+    /// The pattern to look for
+    #[clap(long)]
+    pattern: String,
+
+    /// The path to the file to read
+    #[clap(short, long, parse(from_os_str))]
+    path: Option<PathBuf>,
+
+    #[clap(flatten)]
+    verbose: clap_verbosity_flag::Verbosity,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
 }
