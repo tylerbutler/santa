a8a6179f9b81bef516268686d25896d4f954b4a4
Tyler Butler <tyler@tylerbutler.com>
2025-11-17 11:34:51 -0800

    move data files into src/

--- crates/santa-cli/src/data/constants.rs
+++ crates/santa-cli/src/data/constants.rs
@@ -1,3 +1,3 @@
-pub static BUILTIN_SOURCES: &str = include_str!("../../data/sources.ccl");
-pub static BUILTIN_PACKAGES: &str = include_str!("../../data/known_packages.ccl");
-pub static DEFAULT_CONFIG: &str = include_str!("../../data/santa-config.ccl");
+pub static BUILTIN_SOURCES: &str = include_str!("./sources.ccl");
+pub static BUILTIN_PACKAGES: &str = include_str!("./known_packages.ccl");
+pub static DEFAULT_CONFIG: &str = include_str!("./santa-config.ccl");
